69bc9b21721d2d6ee30c6e254109a14ee426150e
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-05-02 21:14:12 +0200

    Add Tracking Tags

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -8,3 +8,13 @@ $nav = new NavService('footer');
 <footer class="og-footer-classic a-bg--dark l-flex l-flex--wrap a-pad u-shadow--top">
     <?php echo $nav->getFooterElements() ?>
 </footer>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128687860-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-128687860-2');
+</script>
