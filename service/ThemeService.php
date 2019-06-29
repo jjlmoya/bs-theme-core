042fd46335d11f6ea0c872334f279110730e8581
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-06-29 23:01:08 +0200

    Add Brand Service

---
+++ service/ThemeService.php
@@ -0,0 +1,31 @@
+<?php
+
+class ThemeService
+{
+    public static $brands = array(
+        "aviator",
+        "blueberry",
+        "cake",
+        "caribbean",
+        "celeste",
+        "clouds",
+        "diable",
+        "eevee",
+        "forest",
+        "graylord",
+        "kino",
+        "lavandula",
+        "lemon",
+        "limeSports",
+        "lollipop",
+        "mint",
+        "nature",
+        "paradiseOrange",
+        "purpleSkies",
+        "reverseSky",
+        "sky",
+        "springMelody",
+        "strawberry",
+        "wood"
+    );
+}
