0436c1dbd24c38dcd90ebefd30c7287a75a34a77
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-16 23:07:45 +0200

    Clean Woocommerce Imports

--- supports/statics.php
+++ supports/statics.php
@@ -23,8 +23,43 @@ function my_deregister_scripts()
     wp_deregister_script('wp-embed');
 }
 
+
+function bs_woocommerce_script_cleaner()
+{
+
+    // Remove the generator tag
+    remove_action('wp_head', array($GLOBALS['woocommerce'], 'generator'));
+    // Unless we're in the store, remove all the cruft!
+    if (!is_woocommerce() && !is_cart() && !is_checkout()) {
+        wp_dequeue_script('selectWoo');
+        wp_deregister_script('selectWoo');
+        wp_dequeue_script('wc-add-payment-method');
+        wp_dequeue_script('wc-lost-password');
+        wp_dequeue_script('wc_price_slider');
+        wp_dequeue_script('wc-single-product');
+        wp_dequeue_script('wc-add-to-cart');
+        wp_dequeue_script('wc-cart-fragments');
+        wp_dequeue_script('wc-credit-card-form');
+        wp_dequeue_script('wc-checkout');
+        wp_dequeue_script('wc-add-to-cart-variation');
+        wp_dequeue_script('wc-single-product');
+        wp_dequeue_script('wc-cart');
+        wp_dequeue_script('wc-chosen');
+        wp_dequeue_script('woocommerce');
+        wp_dequeue_script('prettyPhoto');
+        wp_dequeue_script('prettyPhoto-init');
+        wp_dequeue_script('jquery-blockui');
+        wp_dequeue_script('jquery-placeholder');
+        wp_dequeue_script('jquery-payment');
+        wp_dequeue_script('fancybox');
+        wp_dequeue_script('jqueryui');
+    }
+}
+
+add_action('wp_enqueue_scripts', 'bs_woocommerce_script_cleaner', 99);
 add_action('after_setup_theme', 'bs_seo_setup');
 add_action('wp_footer', 'my_deregister_scripts');
 add_action('wp_enqueue_scripts', 'bs_seo_scripts');
+add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
 
