ff9fd42e0d9582cdd0c35741be3ab38c139ee2a7
jjlmoya <juanjo1989@gmail.com>
2019-07-31 22:01:03 +0200

    Add Logo Support to Theme

--- template-parts/header.php
+++ template-parts/header.php
@@ -1,13 +1,23 @@
 <?php
-$image = new ImageOptimizer('logo.png', 'Bonseo', 'Bonseo', 'Logo Bonseo');
-$image->modifier = 'l-column--1-1 l-position--absolute  a-pad-0';
-$image->imageModifier = 'a-image l-position--absolute l-position--absolute--center-x og-logo-inline__image';
+
 ?>
 <div class="og-logo-inline
     a-bg--mono-0
     l-flex u-layer-2 u-shadow--bottom l-position">
     <a href="/">
-        <?php echo $image->render(); ?>
+        <?php
+        if (function_exists('the_custom_logo')) {
+            $custom_logo_id = get_theme_mod('custom_logo');
+            $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+            if (!empty($logo))
+                $image = new ImageOptimizer('', 'Bonseo', 'Bonseo', 'Logo Bonseo');
+                $image->modifier = 'l-column--1-1 l-position--absolute  a-pad-0';
+                $image->fullUrl = esc_url($logo[0]);
+                $image->imageModifier = 'a-image l-position--absolute l-position--absolute--center-x og-logo-inline__image';{
+                echo $image->render();
+            }
+        }
+        ?>
     </a>
     <nav class="og-logo-inline__navigation og-logo-inline__navigation--left l-flex l-flex-item--align-center l-column--1-1 l-flex--justify-end">
         <a href="/servicios"
